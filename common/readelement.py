7c3baf4b7d264bd77cd8bb554a5991068270a5ed
wxhou <wxhou@yunjinginc.com>
2023-11-20 16:11:31 +0800

    update

--- common/readelement.py
+++ common/readelement.py
@@ -8,7 +8,7 @@ from config.conf import settings
 class Element(object):
     """获取元素"""
 
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.file_name = '%s.yaml' % name
         self.element_path = os.path.join(settings.ELEMENT_PATH, self.file_name)
         if not os.path.exists(self.element_path):
@@ -16,13 +16,17 @@ class Element(object):
         with open(self.element_path, encoding='utf-8') as f:
             self.data = yaml.safe_load(f)
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str):
         """获取属性"""
-        data = self.data.get(item)
-        if data:
-            name, value = data.split('==')
-            return name, value
-        raise ArithmeticError("{}中不存在关键字：{}".format(self.file_name, item))
+        if not item:
+            raise KeyError("关键字不正确")
+        data: str = self.data.get(item)
+        if not data:
+            raise ArithmeticError("{}中不存在关键字：{}".format(self.file_name, item))
+        if '==' not in data:
+            raise ValueError("{}中{}关键字数据不正确".format(self.file_name, item))
+        name, value = data.split('==')
+        return name, value
 
 
 if __name__ == '__main__':
--- page/webpage.py
+++ page/webpage.py
@@ -4,6 +4,8 @@
 selenium基类
 本文件存放了selenium基类的封装方法
 """
+from typing import Union, Callable
+from selenium.webdriver import Chrome, Firefox, Edge, Ie, Opera, Safari, Remote
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
@@ -17,10 +19,9 @@ class WebPage(object):
     """selenium基类"""
 
     def __init__(self, driver):
-        # self.driver = webdriver.Chrome()
-        self.driver = driver
-        self.timeout = 20
-        self.wait = WebDriverWait(self.driver, self.timeout)
+        self.driver: Union[Chrome, Firefox, Edge, Ie, Opera, Safari, Remote] = driver
+        self.timeout: int = 20
+        self.wait: WebDriverWait = WebDriverWait(self.driver, self.timeout)
 
     def get_url(self, url):
         """打开网址并验证"""
@@ -34,28 +35,29 @@ class WebPage(object):
             raise TimeoutException("打开%s超时请检查网络或网址服务器" % url)
 
     @staticmethod
-    def element_locator(func, locator):
+    def element_locator(func: Callable, locator: tuple):
         """元素定位器"""
+        logger.info("locator is:{}".format(locator))
         name, value = locator
         return func(settings.LOCATE_MODE[name], value)
 
-    def find_element(self, locator):
+    def find_element(self, locator: tuple):
         """寻找单个元素"""
         return WebPage.element_locator(lambda *args: self.wait.until(
             EC.presence_of_element_located(args)), locator)
 
-    def find_elements(self, locator):
+    def find_elements(self, locator: tuple):
         """查找多个相同的元素"""
         return WebPage.element_locator(lambda *args: self.wait.until(
             EC.presence_of_all_elements_located(args)), locator)
 
-    def elements_num(self, locator):
+    def elements_num(self, locator: tuple) -> int:
         """获取相同元素的个数"""
         number = len(self.find_elements(locator))
         logger.info("相同元素：{}".format((locator, number)))
         return number
 
-    def input_text(self, locator, txt):
+    def input_text(self, locator: tuple, txt: str):
         """输入(输入前先清空)"""
         sleep(0.5)
         ele = self.find_element(locator)
@@ -63,13 +65,13 @@ class WebPage(object):
         ele.send_keys(txt)
         logger.info("输入文本：{}".format(txt))
 
-    def click(self, locator):
+    def click(self, locator: tuple):
         """点击"""
         self.find_element(locator).click()
         sleep()
         logger.info("点击元素：{}".format(locator))
 
-    def is_visible(self, locator):
+    def is_visible(self, locator: tuple) -> bool:
         """元素是否可见"""
         try:
             ele = WebPage.element_locator(lambda *args: self.visible_obj.until(
@@ -80,7 +82,7 @@ class WebPage(object):
         except TimeoutException:
             return False
 
-    def element_text(self, locator):
+    def element_text(self, locator: tuple):
         """获取当前的text"""
         _text = self.find_element(locator).text
         logger.info("获取文本：{}".format(_text))
